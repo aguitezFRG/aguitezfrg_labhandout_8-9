71f3ea1b516e54642888c3dddc1fb7e7e723e020
aguitezFRG <fpaguitez@up.edu.ph>
2024-04-25 14:54:24 +0800

    test/chore: uploading and finishing the lab handout 8 sample/test code

---
+++ react-basics/src/App.js
@@ -0,0 +1,25 @@
+import SubjectList from './SubjectList'
+
+
+function App() {
+  return (
+    <div>
+        <SubjectList data={CMSCSubjects} title={'Computer Science Subjects'} />
+        <SubjectList data={CHEMSubjects} title={'Chemistry'} />
+    </div>
+  );
+}
+
+export default App;
+
+const CMSCSubjects = [
+  { code: "CMSC100", description: "Web Programming", id: 1 },
+  { code: "CMSC150", description: "Scientific Computation", id: 2 },
+  { code: "CMSC22", description: "Object-Oriented Programming", id: 3 },
+];
+
+const CHEMSubjects = [
+  { code: "CHEM100", description: "Introduction to Organic Chemistry", id: 1 },
+  { code: "CHEM10", description: "Chemical Kinetics", id: 2 },
+  { code: "CHEM11", description: "Introduction to Biochemistry", id: 3 },
+];
